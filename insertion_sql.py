ea8bbea80ffbcc1701c507a2262ca3063b1e256d
slvg01 <sylvain.legay@gmail.com>
2025-03-20 18:01:42 +0100

    sql insertion script

---
+++ insertion_sql.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import requests
+#import pyodbc
+import json
+
+
+URL = "https://opendata.elia.be/api/explore/v2.1/catalog/datasets/ods177/records?limit=2&offset=0"
+
+def extract_data (URL):
+    response = requests.get(URL)
+    if response.status_code != 200:
+        print('Failed to get data:', response.status_code)
+        return
+    else:
+        print('Successfully got data:', response.status_code)
+        data = response.json()
+        print(json.dumps(data, indent=2)) # pretty print
+        records = data['results']
+        df = pd.json_normalize(records)
+        print(df)
+        return df
+    
+
+
+extract_data(URL)
